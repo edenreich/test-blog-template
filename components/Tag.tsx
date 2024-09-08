57a89c885087ff8174ec0b5d92c8eb0462a6a126
Eden Reich <eden.reich@gmail.com>
2024-09-08 19:02:57 +0000

    Organize the tags differently
    Put them all at the top of the page as a clickable buttons

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
 import { slug } from 'github-slugger'
-interface Props {
+
+interface TagProps {
   text: string
 }
 
-const Tag = ({ text }: Props) => {
+const Tag = ({ text }: TagProps) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:text-primary-400 dark:hover:text-primary-300"
     >
-      {text.split(' ').join('-')}
+      #{text}
     </Link>
   )
 }
--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -68,7 +68,6 @@ export default function ListLayoutWithTags({
   initialDisplayPosts = [],
   pagination,
 }: ListLayoutProps) {
-  const pathname = usePathname()
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
@@ -83,42 +82,25 @@ export default function ListLayoutWithTags({
             {title}
           </h1>
         </div>
-        <div className="flex sm:space-x-24">
-          <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
-            <div className="px-6 py-4">
-              {pathname.startsWith('/blog') ? (
-                <h3 className="font-bold uppercase text-primary-500">All Posts</h3>
-              ) : (
-                <Link
-                  href={`/blog`}
-                  className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
-                >
-                  All Posts
-                </Link>
-              )}
-              <ul>
-                {sortedTags.map((t) => {
-                  return (
-                    <li key={t} className="my-3">
-                      {pathname.split('/tags/')[1] === slug(t) ? (
-                        <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
-                          {`${t} (${tagCounts[t]})`}
-                        </h3>
-                      ) : (
-                        <Link
-                          href={`/tags/${slug(t)}`}
-                          className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
-                          aria-label={`View posts tagged ${t}`}
-                        >
-                          {`${t} (${tagCounts[t]})`}
-                        </Link>
-                      )}
-                    </li>
-                  )
-                })}
-              </ul>
+        {/* Tags section for both mobile and desktop */}
+        <div className="flex flex-wrap">
+          {sortedTags.map((t) => (
+            <div
+              key={t}
+              className="mb-2 mr-5 mt-2 rounded-lg bg-white p-2 shadow-lg dark:bg-gray-800"
+            >
+              <Tag text={t} />
+              <Link
+                href={`/tags/${slug(t)}`}
+                className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                aria-label={`View posts tagged ${t}`}
+              >
+                {` (${tagCounts[t]})`}
+              </Link>
             </div>
-          </div>
+          ))}
+        </div>
+        <div className="flex sm:space-x-24">
           <div>
             <ul>
               {displayPosts.map((post) => {
