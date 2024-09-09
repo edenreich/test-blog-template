437a1143c5a6e6624f0afc139b43dd82b1f3108f
Eden Reich <eden.reich@gmail.com>
2024-09-09 22:15:42 +0000

    Keep it simple, just use standard video controls

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -5,7 +5,6 @@ import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
-import Video from '@/components/Video'
 
 export const components: MDXComponents = {
   Image,
@@ -14,5 +13,4 @@ export const components: MDXComponents = {
   pre: Pre,
   table: TableWrapper,
   BlogNewsletterForm,
-  Video: Video,
 }
